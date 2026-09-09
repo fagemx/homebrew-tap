215677119ec17759fa5e9408b3de7f83360b17a0
fagemx <fagemtez@gmail.com>
2026-09-09 10:48:31 +0800

    edda 0.6.0

--- Formula/edda.rb
+++ Formula/edda.rb
@@ -1,26 +1,26 @@
 class Edda < Formula
   desc "Decision memory for coding agents"
   homepage "https://github.com/fagemx/edda"
-  version "0.1.0"
+  version "0.6.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/fagemx/edda/releases/download/v#{version}/edda-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "68a0753f694596a636b62e4f5284cf19fdf1ccf6741c79ea617ba76d4c32da61"
+      sha256 "75bade6f1098da3a61f30b183f5b6d77d4811da9658eedaf609de061e6631b6c"
     else
       url "https://github.com/fagemx/edda/releases/download/v#{version}/edda-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "d207bbb143f413e1f0a9f6dca699bc45b1b1d65a2d485f3533489a417ccce434"
+      sha256 "8ff9171ffe059d90dbced294f59f3fe011ea941b3ce6c80a1fc01de1d5d2dc7a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/fagemx/edda/releases/download/v#{version}/edda-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "898f58a29a7ae37ea0e00083b3691901f1596f1b85926873a95e6c75dabb63f2"
+      sha256 "1694efcf91865d6c06b604324d3de33fc8d676a0d3b60ad35a69305c322ac7d1"
     else
       url "https://github.com/fagemx/edda/releases/download/v#{version}/edda-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "5485cf7e4a02a10eb0de609425a93083f5fd77cd6a138243aa88b74f392caa64"
+      sha256 "3607eed7d635d0850058de8425e4e94fbd2028a57d37144734d06e5711f3c2bf"
     end
   end
 
